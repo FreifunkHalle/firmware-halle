d59fb8d271c499a7c7221d9b1ab2f18504903b0d
Matthias <freifunk@3dfxatwork.de>
2014-09-17 19:47:31 +0200

    resolve merge conflicts and update repos

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 # default parameters for Makefile
 TARGET=ar71xx
 OPENWRT_SRC=git://git.openwrt.org/openwrt.git
-OPENWRT_COMMIT=cfff55dd33b63098efd904936c802411ede39c44
+OPENWRT_COMMIT=d59bd75fa777af92a4f116b51cae66ab9824137e
 MAKE_ARGS="IGNORE_ERRORS=m"
